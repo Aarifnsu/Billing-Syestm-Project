a32d54882bb594c3f7e1de8897fd2c5e2914f1a6
Aarifnsu <57924838+Aarifnsu@users.noreply.github.com>
2021-01-11 09:46:28 +0600

    Add files via upload

---
+++ CODE/Resturant_Billing_Syestem/src/ResturantBill/SubberClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ResturantBill;
+
+/**
+ *
+ * @author Admin
+ */
+public class SubberClass {
+ public doubble KACCHI;
+ public doubble BIRIANY;
+ public doubble KHICHURI;
+ public doubble CHIKENGURGER;
+ public doubble BEEFGURGER;
+ public doubble MEALS;
+ public doubble DRINK;
+ public doubble TOTALTAKA;
+                    
+}
